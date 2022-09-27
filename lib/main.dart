810cd2c6b43e45cb6082af1ff1f372c0f9860d4e
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-09-28 08:49:00 +0900

    Firebase の初期化と設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
